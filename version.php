ffef239c3ed0c21a5851da3016af274b80fe5b67
Valery Fremaux <valery.fremaux@gmail.com>
2025-04-28 12:22:34 +0200

    %1

--- version.php
+++ version.php
@@ -18,7 +18,6 @@
  * Version details.
  *
  * @package     block_profileselectorhtml
- * @category    blocks
  * @author      Valery Fremaux <valery.fremaux@gmail.com>
  * @copyright   2012 onwards Valery Fremaux (http://www.mylearningfactory.com)
  * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
@@ -26,13 +25,13 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2016083100;        // The current plugin version (Date: YYYYMMDDXX).
-$plugin->requires  = 2022041200;        // Requires this Moodle version.
+$plugin->version   = 2025011400;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->requires  = 2022112801;        // Requires this Moodle version.
 $plugin->component = 'block_profileselectorhtml';  // Full name of the plugin (used for diagnostics).
-$plugin->maturity = MATURITY_STABLE;
-$plugin->release = '4.0.0 (build 2016083100)';
-$plugin->supported = [40,40];
+$plugin->maturity = MATURITY_RC;
+$plugin->release = '4.5.0 (build 2016083100)';
+$plugin->supported = [401, 405];
 
 // Non moodle attributes.
-$plugin->codeincrement = '4.0.0000';
+$plugin->codeincrement = '4.5.0000';
 $plugin->privacy = 'public';
